3a6067197b31ad03c45e310b7c51ea7d752c3219
ryanhausen <rhausen@ucsc.edu>
2023-05-08 17:34:17 -0400

    2018 read in/parsed with networkx, need to eval another year

--- tip_demo/evaluate_nih_2018.py
+++ tip_demo/evaluate_nih_2018.py
@@ -1,14 +1,17 @@
 import json
 import matplotlib.pyplot as plt
-
+import networkx as nx
 
 from nih_award import NihAward
 from nih_pi import NihPi
 
+
 try:
     from tip_demo.algorithm_data_model import Author, Paper
+    from tip_demo.algorithm import convert_paper_to_graph
 except ImportError:
     from algorithm_data_model import Author, Paper
+    from algorithm import convert_paper_to_graph
 
 def main():
 
@@ -20,13 +23,40 @@ def main():
     with open("awards-2019.json", "r") as f:
         ml_awards.extend(list(map(NihAward.from_nih_api, json.load(f))))
 
-    ml_authors = list(set(ml_authors))
+    ml_authors = dict(map(
+        lambda x: (x.profile_id, x.to_graph_object()),
+        set(ml_authors)
+    ))
 
-    for award in ml_awards:
-        print(award.to_graph_object())
+    get_authors = lambda ids: [ml_authors[i] for i in ids]
+
+    paper_graphs = list(
+        map(
+            lambda award: convert_paper_to_graph(
+                get_authors(award.principal_investigators),
+                award.to_graph_object()
+            ),
+            set(ml_awards)
+        )
+    )
+
+    print(f"Found {len(paper_graphs)} papers")
+    print("total nodes:", sum(map(nx.number_of_nodes, paper_graphs)))
+
+    composed_graph = nx.compose_all(paper_graphs)
+    print("composed nodes:", nx.number_of_nodes(composed_graph))
 
 
-
+    f, ax = plt.subplots(figsize=(40, 40))
+    nx.draw(
+        composed_graph,
+        ax=ax,
+        font_size=6,
+        node_size=10,
+        with_labels=False,
+        node_color=[n[1]["color"] for n in composed_graph.nodes(data=True)],
+    )
+    plt.show()
 
 if __name__ == "__main__":
     main()
